8148c3df6131208b23dda0335726e17ed941de73
Clarisse Gagnebin <clarisse.gagnebin@heig-vd.ch>
2021-10-12 15:08:22 +0200

    Modification des lignes 63 et 80. Suppression d'une tâche (ligne 63). Affichage de toutes les tâches (ligne 80).

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ define('DB_USER', 'todolist');
 define('DB_PASS', 'chAngeMeN0w!');
 define('DB_NAME', 'todolist');
 define('DB_HOST', '127.0.0.1');
-define('DB_PORT', '3306');
+define('DB_PORT', '8889');
 
 $db = new PDO('mysql:host='.DB_HOST.';port='.DB_PORT.';dbname='.DB_NAME, DB_USER, DB_PASS);
 $items = array();
@@ -60,7 +60,7 @@ if (isset($_POST['action'])) {
 
       $id = $_POST['id'];
       if(is_numeric($id)) {
-        $deleteQuery = ''; // IMPLEMENT ME
+        $deleteQuery = "DELETE FROM todo WHERE todo.id=$id"; 
         if(!$db->query($deleteQuery)) {
           die(print_r($db->errorInfo(), true));
         }
@@ -77,7 +77,7 @@ if (isset($_POST['action'])) {
 /**
  * Select all tasks from the database.
  */
-$selectQuery = ''; // IMPLEMENT ME
+$selectQuery = 'SELECT * FROM todo';
 $items = $db->query($selectQuery);
 ?>
 
